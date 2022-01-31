bcb38ac1d1bf15a5bd63cbed05fa720d0edbae5f
Pierre Bretéché <pbreteche@dawan.fr>
2022-01-31 17:08:56 +0100

    Annotation syntax for Routes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
 
+    /**
+     * @Route("/")
+     */
     public function index(): Response
     {
         return $this->json(['status' => 'OK']);
