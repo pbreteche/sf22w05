6f516ccd3e1f0403ddb0ef3e9852a5ca72a56ae1
Pierre Bretéché <pbreteche@dawan.fr>
2022-02-01 16:28:18 +0100

    Add post create action

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -44,5 +46,26 @@ class PostController extends AbstractController
         ];
 
         return $this->json($normalizedPost);
+    }
+
+    /**
+     * @Route("/", methods="POST")
+     */
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $post = (new Post())
+            ->setTitle($data['title'])
+            ->setBody($data['body'])
+            ->setCreatedAt(new \DateTimeImmutable())
+        ;
+
+        $manager->persist($post);
+        $manager->flush();
+
+        $content = ['location' => $this->generateUrl('app_post_show', ['id' => $post->getId()])];
+
+        return $this->json($content, Response::HTTP_CREATED);
     }
 }
